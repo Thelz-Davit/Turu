597fe56d9ce022bd4e5a251c9530ad2434079c5b
Thelzyboi <Davitra03@gmail.com>
2023-03-30 19:42:42 +0700

    First Commit, creating necessary files

---
+++ layouts/navbar.php
@@ -0,0 +1,31 @@
+<section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="image/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img class="d-inline-block align-text-top" src="/image/Turu.png" alt="" width="60" height="48">
+            </a>
+            <button class="navbar-toggler d-lg-none" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <form class="d-flex my-2 my-lg-0 ms-auto">
+                    <a href="welcome.php?logout=1" id="logout-btn" class="btn btn-outline-danger my-2 my-sm-0">Logout</a>
+                </form>
+            </div>
+      </div>
+    </nav>
+</section>
